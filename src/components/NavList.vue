e455dc51905e930f26a6c6f98e118f5f5a60aa24
Nicolò Catania <101711650+NicoloAkaCat@users.noreply.github.com>
2024-07-24 12:19:33 +0200

    modal UI improvements + fix z-index

--- src/components/NavList.vue
+++ src/components/NavList.vue
@@ -2,7 +2,7 @@
 
 <template>
   <ul
-    class="absolute right-8 top-full z-50 mt-10 flex w-max flex-col items-center justify-center gap-4 rounded-lg border-2 border-slate-200 bg-gray-800 p-6 text-center shadow-2xl"
+    class="absolute right-8 top-full z-40 mt-10 flex w-max flex-col items-center justify-center gap-4 rounded-lg border-2 border-slate-200 bg-gray-800 p-6 text-center shadow-2xl"
   >
     <slot></slot>
   </ul>
--- src/components/modals/ModalComponent.vue
+++ src/components/modals/ModalComponent.vue
@@ -12,10 +12,12 @@ defineEmits<{
     <Transition name="modal">
       <div
         v-if="show"
-        class="absolute left-0 top-0 flex min-h-screen w-svw items-center justify-center bg-black bg-opacity-45"
+        class="absolute left-0 top-0 z-50 flex min-h-screen w-svw items-center justify-center bg-black bg-opacity-45"
         @click="$emit('closed')"
       >
-        <div class="w-max rounded-xl bg-gray-800 p-10 text-center shadow-lg md:p-12">
+        <div
+          class="m-10 w-full rounded-xl bg-gray-800 p-10 text-center shadow-lg md:w-6/12 md:p-12 lg:w-2/6"
+        >
           <slot></slot>
         </div>
       </div>
